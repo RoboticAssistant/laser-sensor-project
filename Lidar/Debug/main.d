721d2fbdcc22b96c5c8ebb2ef7025e61eb949ab5
asmita.boo <asmita.boo@gmail>
2017-03-05 20:09:17 -0800

    Code Composer Studio project for Laser Range finder added with basic I2C communication

---
+++ Lidar/Debug/main.d
@@ -0,0 +1,93 @@
+# FIXED
+
+main.obj: ../main.c
+main.obj: /Applications/ti/ccsv6/ccs_base/arm/include/msp.h
+main.obj: /Applications/ti/ccsv6/ccs_base/arm/include/msp432p401r.h
+main.obj: /Applications/ti/ccsv6/tools/compiler/arm_15.12.3.LTS/include/stdint.h
+main.obj: /Applications/ti/ccsv6/ccs_base/arm/include/msp_compatibility.h
+main.obj: /Applications/ti/ccsv6/ccs_base/arm/include/CMSIS/cmsis_ccs.h
+main.obj: /Applications/ti/ccsv6/ccs_base/arm/include/msp432p401r_classic.h
+main.obj: /Applications/ti/ccsv6/ccs_base/arm/include/CMSIS/core_cm4.h
+main.obj: /Applications/ti/ccsv6/ccs_base/arm/include/CMSIS/core_cmInstr.h
+main.obj: /Applications/ti/ccsv6/ccs_base/arm/include/CMSIS/core_cmFunc.h
+main.obj: /Applications/ti/ccsv6/ccs_base/arm/include/CMSIS/core_cmSimd.h
+main.obj: /Applications/ti/ccsv6/ccs_base/arm/include/system_msp432p401r.h
+main.obj: /Users/Boogie/ti/tirex-content/msp/msp432ware__3.50.00.02/driverlib/driverlib/MSP432P4xx/i2c.h
+main.obj: /Applications/ti/ccsv6/tools/compiler/arm_15.12.3.LTS/include/stdbool.h
+main.obj: /Users/Boogie/ti/tirex-content/msp/msp432ware__3.50.00.02/driverlib/driverlib/MSP432P4xx/eusci.h
+main.obj: /Users/Boogie/ti/tirex-content/msp/msp432ware__3.50.00.02/driverlib/driverlib/MSP432P4xx/driverlib.h
+main.obj: /Users/Boogie/ti/tirex-content/msp/msp432ware__3.50.00.02/driverlib/driverlib/MSP432P4xx/adc14.h
+main.obj: /Users/Boogie/ti/tirex-content/msp/msp432ware__3.50.00.02/driverlib/driverlib/MSP432P4xx/aes256.h
+main.obj: /Users/Boogie/ti/tirex-content/msp/msp432ware__3.50.00.02/driverlib/driverlib/MSP432P4xx/comp_e.h
+main.obj: /Users/Boogie/ti/tirex-content/msp/msp432ware__3.50.00.02/driverlib/driverlib/MSP432P4xx/cpu.h
+main.obj: /Users/Boogie/ti/tirex-content/msp/msp432ware__3.50.00.02/driverlib/driverlib/MSP432P4xx/crc32.h
+main.obj: /Users/Boogie/ti/tirex-content/msp/msp432ware__3.50.00.02/driverlib/driverlib/MSP432P4xx/cs.h
+main.obj: /Users/Boogie/ti/tirex-content/msp/msp432ware__3.50.00.02/driverlib/driverlib/MSP432P4xx/dma.h
+main.obj: /Users/Boogie/ti/tirex-content/msp/msp432ware__3.50.00.02/driverlib/driverlib/MSP432P4xx/flash.h
+main.obj: /Users/Boogie/ti/tirex-content/msp/msp432ware__3.50.00.02/driverlib/driverlib/MSP432P4xx/fpu.h
+main.obj: /Users/Boogie/ti/tirex-content/msp/msp432ware__3.50.00.02/driverlib/driverlib/MSP432P4xx/gpio.h
+main.obj: /Users/Boogie/ti/tirex-content/msp/msp432ware__3.50.00.02/driverlib/driverlib/MSP432P4xx/interrupt.h
+main.obj: /Users/Boogie/ti/tirex-content/msp/msp432ware__3.50.00.02/driverlib/driverlib/MSP432P4xx/mpu.h
+main.obj: /Users/Boogie/ti/tirex-content/msp/msp432ware__3.50.00.02/driverlib/driverlib/MSP432P4xx/pcm.h
+main.obj: /Users/Boogie/ti/tirex-content/msp/msp432ware__3.50.00.02/driverlib/driverlib/MSP432P4xx/pmap.h
+main.obj: /Users/Boogie/ti/tirex-content/msp/msp432ware__3.50.00.02/driverlib/driverlib/MSP432P4xx/pss.h
+main.obj: /Users/Boogie/ti/tirex-content/msp/msp432ware__3.50.00.02/driverlib/driverlib/MSP432P4xx/ref_a.h
+main.obj: /Users/Boogie/ti/tirex-content/msp/msp432ware__3.50.00.02/driverlib/driverlib/MSP432P4xx/reset.h
+main.obj: /Users/Boogie/ti/tirex-content/msp/msp432ware__3.50.00.02/driverlib/driverlib/MSP432P4xx/rom.h
+main.obj: /Users/Boogie/ti/tirex-content/msp/msp432ware__3.50.00.02/driverlib/driverlib/MSP432P4xx/rom_map.h
+main.obj: /Users/Boogie/ti/tirex-content/msp/msp432ware__3.50.00.02/driverlib/driverlib/MSP432P4xx/rtc_c.h
+main.obj: /Users/Boogie/ti/tirex-content/msp/msp432ware__3.50.00.02/driverlib/driverlib/MSP432P4xx/spi.h
+main.obj: /Users/Boogie/ti/tirex-content/msp/msp432ware__3.50.00.02/driverlib/driverlib/MSP432P4xx/sysctl.h
+main.obj: /Users/Boogie/ti/tirex-content/msp/msp432ware__3.50.00.02/driverlib/driverlib/MSP432P4xx/systick.h
+main.obj: /Users/Boogie/ti/tirex-content/msp/msp432ware__3.50.00.02/driverlib/driverlib/MSP432P4xx/timer32.h
+main.obj: /Users/Boogie/ti/tirex-content/msp/msp432ware__3.50.00.02/driverlib/driverlib/MSP432P4xx/timer_a.h
+main.obj: /Users/Boogie/ti/tirex-content/msp/msp432ware__3.50.00.02/driverlib/driverlib/MSP432P4xx/uart.h
+main.obj: /Users/Boogie/ti/tirex-content/msp/msp432ware__3.50.00.02/driverlib/driverlib/MSP432P4xx/wdt_a.h
+main.obj: /Applications/ti/ccsv6/tools/compiler/arm_15.12.3.LTS/include/string.h
+main.obj: /Applications/ti/ccsv6/tools/compiler/arm_15.12.3.LTS/include/linkage.h
+
+../main.c: 
+/Applications/ti/ccsv6/ccs_base/arm/include/msp.h: 
+/Applications/ti/ccsv6/ccs_base/arm/include/msp432p401r.h: 
+/Applications/ti/ccsv6/tools/compiler/arm_15.12.3.LTS/include/stdint.h: 
+/Applications/ti/ccsv6/ccs_base/arm/include/msp_compatibility.h: 
+/Applications/ti/ccsv6/ccs_base/arm/include/CMSIS/cmsis_ccs.h: 
+/Applications/ti/ccsv6/ccs_base/arm/include/msp432p401r_classic.h: 
+/Applications/ti/ccsv6/ccs_base/arm/include/CMSIS/core_cm4.h: 
+/Applications/ti/ccsv6/ccs_base/arm/include/CMSIS/core_cmInstr.h: 
+/Applications/ti/ccsv6/ccs_base/arm/include/CMSIS/core_cmFunc.h: 
+/Applications/ti/ccsv6/ccs_base/arm/include/CMSIS/core_cmSimd.h: 
+/Applications/ti/ccsv6/ccs_base/arm/include/system_msp432p401r.h: 
+/Users/Boogie/ti/tirex-content/msp/msp432ware__3.50.00.02/driverlib/driverlib/MSP432P4xx/i2c.h: 
+/Applications/ti/ccsv6/tools/compiler/arm_15.12.3.LTS/include/stdbool.h: 
+/Users/Boogie/ti/tirex-content/msp/msp432ware__3.50.00.02/driverlib/driverlib/MSP432P4xx/eusci.h: 
+/Users/Boogie/ti/tirex-content/msp/msp432ware__3.50.00.02/driverlib/driverlib/MSP432P4xx/driverlib.h: 
+/Users/Boogie/ti/tirex-content/msp/msp432ware__3.50.00.02/driverlib/driverlib/MSP432P4xx/adc14.h: 
+/Users/Boogie/ti/tirex-content/msp/msp432ware__3.50.00.02/driverlib/driverlib/MSP432P4xx/aes256.h: 
+/Users/Boogie/ti/tirex-content/msp/msp432ware__3.50.00.02/driverlib/driverlib/MSP432P4xx/comp_e.h: 
+/Users/Boogie/ti/tirex-content/msp/msp432ware__3.50.00.02/driverlib/driverlib/MSP432P4xx/cpu.h: 
+/Users/Boogie/ti/tirex-content/msp/msp432ware__3.50.00.02/driverlib/driverlib/MSP432P4xx/crc32.h: 
+/Users/Boogie/ti/tirex-content/msp/msp432ware__3.50.00.02/driverlib/driverlib/MSP432P4xx/cs.h: 
+/Users/Boogie/ti/tirex-content/msp/msp432ware__3.50.00.02/driverlib/driverlib/MSP432P4xx/dma.h: 
+/Users/Boogie/ti/tirex-content/msp/msp432ware__3.50.00.02/driverlib/driverlib/MSP432P4xx/flash.h: 
+/Users/Boogie/ti/tirex-content/msp/msp432ware__3.50.00.02/driverlib/driverlib/MSP432P4xx/fpu.h: 
+/Users/Boogie/ti/tirex-content/msp/msp432ware__3.50.00.02/driverlib/driverlib/MSP432P4xx/gpio.h: 
+/Users/Boogie/ti/tirex-content/msp/msp432ware__3.50.00.02/driverlib/driverlib/MSP432P4xx/interrupt.h: 
+/Users/Boogie/ti/tirex-content/msp/msp432ware__3.50.00.02/driverlib/driverlib/MSP432P4xx/mpu.h: 
+/Users/Boogie/ti/tirex-content/msp/msp432ware__3.50.00.02/driverlib/driverlib/MSP432P4xx/pcm.h: 
+/Users/Boogie/ti/tirex-content/msp/msp432ware__3.50.00.02/driverlib/driverlib/MSP432P4xx/pmap.h: 
+/Users/Boogie/ti/tirex-content/msp/msp432ware__3.50.00.02/driverlib/driverlib/MSP432P4xx/pss.h: 
+/Users/Boogie/ti/tirex-content/msp/msp432ware__3.50.00.02/driverlib/driverlib/MSP432P4xx/ref_a.h: 
+/Users/Boogie/ti/tirex-content/msp/msp432ware__3.50.00.02/driverlib/driverlib/MSP432P4xx/reset.h: 
+/Users/Boogie/ti/tirex-content/msp/msp432ware__3.50.00.02/driverlib/driverlib/MSP432P4xx/rom.h: 
+/Users/Boogie/ti/tirex-content/msp/msp432ware__3.50.00.02/driverlib/driverlib/MSP432P4xx/rom_map.h: 
+/Users/Boogie/ti/tirex-content/msp/msp432ware__3.50.00.02/driverlib/driverlib/MSP432P4xx/rtc_c.h: 
+/Users/Boogie/ti/tirex-content/msp/msp432ware__3.50.00.02/driverlib/driverlib/MSP432P4xx/spi.h: 
+/Users/Boogie/ti/tirex-content/msp/msp432ware__3.50.00.02/driverlib/driverlib/MSP432P4xx/sysctl.h: 
+/Users/Boogie/ti/tirex-content/msp/msp432ware__3.50.00.02/driverlib/driverlib/MSP432P4xx/systick.h: 
+/Users/Boogie/ti/tirex-content/msp/msp432ware__3.50.00.02/driverlib/driverlib/MSP432P4xx/timer32.h: 
+/Users/Boogie/ti/tirex-content/msp/msp432ware__3.50.00.02/driverlib/driverlib/MSP432P4xx/timer_a.h: 
+/Users/Boogie/ti/tirex-content/msp/msp432ware__3.50.00.02/driverlib/driverlib/MSP432P4xx/uart.h: 
+/Users/Boogie/ti/tirex-content/msp/msp432ware__3.50.00.02/driverlib/driverlib/MSP432P4xx/wdt_a.h: 
+/Applications/ti/ccsv6/tools/compiler/arm_15.12.3.LTS/include/string.h: 
+/Applications/ti/ccsv6/tools/compiler/arm_15.12.3.LTS/include/linkage.h: 
